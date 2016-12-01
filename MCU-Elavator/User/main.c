8117659f75e14d5788e573d770b6bbe8aaab7b15
herk009 <hridyajit@gmail.com>
2016-12-01 13:46:15 -0800

    large update from school
    gutted a lot of the code to be simplistic. After debugging the basic carriage functions works. All that's left is emergency stop, reset, and USART. 
    -Liam

--- MCU-Elavator/User/main.c
+++ MCU-Elavator/User/main.c
@@ -1,4 +1,5 @@
 /* Include core modules */
+
 #include "stm32f4xx.h"
 #include "main.h"
 /* Include my libraries here */
@@ -10,15 +11,17 @@
 int main(void) {
 	init();
 	
-	bool moving;
+	//char moving;
 	
 	
 	while(1) {
 		update_inputs();		
-		
+		button_display();
+		bcd_display();
 			
-		if(butt_f1||butt_f2_down||butt_f2_up||butt_f3||butt_car_stop||butt_car_reset) { 			//if any button is activated
-			moving = (moving_up|moving_down);
+		//if(butt_f1||butt_f2_down||butt_f2_up||butt_f3||butt_car_stop||butt_car_reset) { 			//if any button is activated
+			//moving = (moving_up|moving_down);
+			/*
 			if(!door_closed) { 																																	//close doors if they're open. probably only open from reset function.
 				door_close();
 			}
@@ -55,21 +58,26 @@ int main(void) {
 				door_open_critical();
 				for( int i = 0; i< 12*door_delay; i++);																							// keep the door open for a longer while
 			}
+			*/
 			
 			
-			else switch(loc_cur) {   																										//react according to which floor the carrige is at
+			//else switch(loc_cur) {   																										//react according to which floor the carrige is at
+			switch(loc_cur) { 
 				case 1 : {																																//it's on the 1st floor
-					if(butt_f1&&loc_floor_1) {																							//if a button calling the first floor was active
-						if(moving) {
+					if(butt_f1 && loc_floor_1) {																							//if a button calling the first floor was active
+						//if(moving) {
 							move_stop();
-						}														
-						butt_f1 = 0;																													//turn off the buttons and open the doors
+						//}														
+						button_display();
 						if(usart_on) {
 							usart_message("F1, arrived\n");
 						}
 						door_cycle();
+						butt_f1 = 0;																													//turn off the buttons and open the doors
+
 					}
-					else if((butt_f2_down||butt_f2_up||butt_f3)&&!moving&&door_closed) {  				//if a higher floor is called, it's not already moving, and it's ready to go
+					//else if((butt_f2_down||butt_f2_up||butt_f3)&&!moving&&door_closed) {  				//if a higher floor is called, it's not already moving, and it's ready to go
+					else if((butt_f2_down||butt_f2_up||butt_f3)) { 
 						if(usart_on) {
 							usart_message("F1, departing\n");
 						}
@@ -80,39 +88,66 @@ int main(void) {
 				
 				
 				case 2 : {																																								//if it's at the 2nd floor
-					if(butt_f2_down&&loc_floor_2&&moving_down) {															//if the carrige is on it's way down and someone at the second floor is going down, pick 'em up
-						if(moving) {
+					if(butt_f2_down && butt_f2_up && loc_floor_2) {															//if the carrige is on it's way down and someone at the second floor is going down, pick 'em up
+						//if(moving) {
+							/*
 							for(int i = 0; i < motor_delay_2; i++) {															//to adjust to the correct position       TODO find the real value
 								update_inputs();
 							}
+							*/
 							move_stop();							
-						}
-						butt_f2_down = 0;
+						//}
+						
+						button_display();
 						if(usart_on) {
 							usart_message("F2, arrived\n");
 						}
 						door_cycle();
+						butt_f2_down = 0;
+						butt_f2_up = 0;
 					}
-					else if((butt_f2_up)&&loc_floor_2&&moving_up) {														//if the carrige is on it's way up and someone at the second floor is going up, pick 'em up
-						if(moving) {
+					else if(butt_f2_down && loc_floor_2 && !moving_up) {															//if the carrige is on it's way down and someone at the second floor is going down, pick 'em up
+						//if(moving) {
+							/*
 							for(int i = 0; i < motor_delay_2; i++) {															//to adjust to the correct position       TODO find the real value
 								update_inputs();
 							}
-							move_stop();						
-						}
-						butt_f2_up = 0;
+							*/
+							move_stop();							
+						//}
+						
+						button_display();
 						if(usart_on) {
 							usart_message("F2, arrived\n");
 						}
 						door_cycle();
+						butt_f2_down = 0;
+					}
+					else if(butt_f2_up && loc_floor_2 && !moving_down) {														//if the carrige is on it's way up and someone at the second floor is going up, pick 'em up
+						//if(moving) {
+							/*for(int i = 0; i < motor_delay_2; i++) {															//to adjust to the correct position       TODO find the real value
+								update_inputs();
+							}
+							*/
+							move_stop();						
+						//}
+						
+						button_display();
+						if(usart_on) {
+							usart_message("F2, arrived\n");
+						}
+						door_cycle();
+						butt_f2_up = 0;
 					}					
-					else if(butt_f1&&!moving&&door_closed) {										 					//if a lower floor is called, it's not already moving, and it's ready to go
+					//else if(butt_f1&&!moving&&door_closed) {										 					//if a lower floor is called, it's not already moving, and it's ready to go
+					else if(butt_f1) {
 						if(usart_on) {
 							usart_message("F2, departing\n");
 						}
 						move_down();
 					}
-					else if(butt_f3&&!moving&&door_closed) {														//if a higher floor is called, it's not already moving, and it's ready to go
+					//else if(butt_f3&&!moving&&door_closed) {														//if a higher floor is called, it's not already moving, and it's ready to go
+					else if(butt_f3) {	
 						if(usart_on) {
 							usart_message("F2, departing\n");
 						}
@@ -123,33 +158,35 @@ int main(void) {
 				
 				
 				case 3 : {																																			//it's on the 3rd floor
-					if(butt_f3&&loc_floor_3) {																										//if the 3rd floor was called
-						if(moving) {
+					if(butt_f3 && loc_floor_3) {																										//if the 3rd floor was called
+						//if(moving) {
 							move_stop();							
-						}						
-						butt_f3 = 0;
-						if(usart_on) {
-							usart_message("F3, arrived\n");
-						}
+						//}						
+						
+						button_display();
+//						if(usart_on) {
+//							usart_message("F3, arrived\n");
+//						}
 						door_cycle();
+						butt_f3 = 0;
 					}	
-					else if((butt_f1||butt_f2_down||butt_f2_up)&&!moving&&door_closed) {					//if a lower floor is called, it's not already moving, and it's ready to go
-						if(usart_on) {
-							usart_message("F3, departing\n");
-						}
+					//else if((butt_f1||butt_f2_down||butt_f2_up)&&!moving&&door_closed) {					//if a lower floor is called, it's not already moving, and it's ready to go
+					else if(butt_f1 || butt_f2_down || butt_f2_up) {
+//						if(usart_on) {
+//							usart_message("F3, departing\n");
+//						}
 						move_down();
 					}
 					break;
 				}
 			}
-		}		
+		//}		
 	}
 	
 	
-	return 0;
 }
 
-bool init(void) { 																																								//itianize all the pins and whatever else
+char init(void) { 																																								//itianize all the pins and whatever else
 	SystemInit();
 	
 	//Input pins
@@ -163,37 +200,45 @@ bool init(void) { 																																								//itianize all the pin
 	TM_GPIO_Init(GPIOC, GPIO_Pin_11 , TM_GPIO_Mode_OUT, TM_GPIO_OType_PP, TM_GPIO_PuPd_NOPULL, TM_GPIO_Speed_High);
 	TM_GPIO_Init(GPIOD, GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_5 , TM_GPIO_Mode_OUT, TM_GPIO_OType_PP, TM_GPIO_PuPd_NOPULL, TM_GPIO_Speed_High);
 	
+	TM_GPIO_Init(GPIOB, GPIO_Pin_10 | GPIO_Pin_12 | GPIO_Pin_14 | GPIO_Pin_11 , TM_GPIO_Mode_OUT, TM_GPIO_OType_PP, TM_GPIO_PuPd_NOPULL, TM_GPIO_Speed_High);
+	
 	//Initialize USART1 at 9600 baud, TX: PB6, RX: PB7
 	TM_USART_Init(USART1, TM_USART_PinsPack_2, 9600);
 	
-	return 0;
-}
+	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_15);
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
 
-bool update_critical(void) {																																			//inputs that always need to be updated
-	if(!butt_car_stop && TM_GPIO_GetInputPinValue(GPIOD, GPIO_Pin_4)) {
-		butt_car_stop = 1;
-	}
-	if(!butt_car_reset && TM_GPIO_GetInputPinValue(GPIOD, GPIO_Pin_6)) {
-		butt_car_reset = 1;
-	}	
 	
 	return 0;
 }
 
-bool update_inputs(void) {																																				//poll all the pins for thier values
-	update_critical();
+//char update_critical(void) {																																			//inputs that always need to be updated
+//	if(!butt_car_stop && TM_GPIO_GetInputPinValue(GPIOD, GPIO_Pin_4)) {
+//		butt_car_stop = 1;
+//	}
+//	if(!butt_car_reset && TM_GPIO_GetInputPinValue(GPIOD, GPIO_Pin_6)) {
+//		butt_car_reset = 1;
+//	}	
+//	
+//	return 0;
+//}
+
+char update_inputs(void) {																																				//poll all the pins for thier values
+	//update_critical();
 	if(loc_floor_1 == TM_GPIO_GetInputPinValue(GPIOC, GPIO_Pin_10)) {																//trigger switch checking for state changes of the location switches
-		loc_floor_1 = ~loc_floor_1;																																		//toggle the variable
+		loc_floor_1 = !loc_floor_1;																																		//toggle the variable
 		loc_cur = 1;																																									//they also set the current floor, or once it leaves it'll be the latest floor
 		bcd_display();
 	}
 	if(loc_floor_2 == TM_GPIO_GetInputPinValue(GPIOC, GPIO_Pin_12)) {																//location sensors are active low, thus the ==, not !=
-		loc_floor_2 = ~loc_floor_2;
+		loc_floor_2 = !loc_floor_2;
 		loc_cur = 2;
 		bcd_display();
 	}
 	if(loc_floor_3 == TM_GPIO_GetInputPinValue(GPIOD, GPIO_Pin_1)) {
-		loc_floor_3 = ~loc_floor_3;
+		loc_floor_3 = !loc_floor_3;
 		loc_cur = 3;
 		bcd_display();
 	}
@@ -201,7 +246,7 @@ bool update_inputs(void) {																																				//poll all the pin
 	
 	if(!butt_f1 && TM_GPIO_GetInputPinValue(GPIOB, GPIO_Pin_3)) {																//only update variable if the pin is high and the varibale isn't already set
 		butt_f1 = 1;																																							//the floor call buttons latch until the request has been satisfied
-																																															//both the buttons inside and out can be connected to this pin
+		TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_10);																																												//both the buttons inside and out can be connected to this pin
 	}
 	if(!butt_f2_up && TM_GPIO_GetInputPinValue(GPIOB, GPIO_Pin_4)) {														//the inside carriage button can be connected to both this pin, and the next one, possibly using diodes or or gates
 		butt_f2_up = 1;
@@ -213,9 +258,11 @@ bool update_inputs(void) {																																				//poll all the pin
 		butt_f3 = 1;
 	}
 	
+	/*
 	if(door_closed != TM_GPIO_GetInputPinValue(GPIOD, GPIO_Pin_2)) {
-		door_closed = ~door_closed;
+		door_closed = !door_closed;
 	}
+	*/
 	
 	//TODO poll for USART password
 
@@ -225,36 +272,36 @@ bool update_inputs(void) {																																				//poll all the pin
 
 
 
-bool move_up(void) {
+char move_up(void) {
 	moving_up = 1;
 	if(usart_on) {
 		usart_message("UP, moving\n");
 	}
 	
-	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_10);																					//TODO fix pins to active low
-	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_10);																					
+	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_14);			
 	return 0;
 }
 
-bool move_down(void) {
+char move_down(void) {
 	moving_down = 1;
 	if(usart_on) {
 		usart_message("DOWN, moving\n");
 	}
-	
-	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_14);																					//TODO fix pins to active low
+	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_10);
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_14);																					
 	
 	return 0;
 } 
 
-bool move_stop(void) {
+char move_stop(void) {
 	moving_up = 0;
 	moving_down = 0;
 	if(usart_on) {
 		usart_message("STOP, stopping\n");
 	}
 	
-	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_10);																					//TODO fix pins to active low
+	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_10);																				
 	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_14);
 	
 	return 0;
@@ -262,12 +309,12 @@ bool move_stop(void) {
 
 
 
-bool usart_message(char* str) {
+char usart_message(char* str) {
 	TM_USART_Puts(USART1, str);
 	return 0;
 }
 
-bool bcd_display(void){
+char bcd_display(void){
 	switch(loc_cur) {
 		case 1 : {
 			TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_5);																				
@@ -289,39 +336,53 @@ bool bcd_display(void){
 	return 0;
 }
 
-bool door_open(void){
+char door_open(void){
 	if(usart_on) {
 		usart_message("OPEN, doors\n");
 	}
 	
 	door_closed = 0;
-		
-	TM_GPIO_SetPinLow(GPIOC, GPIO_Pin_11);																				
-	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
+	
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_15);
 	for( int i = 0; i<door_delay ; i++){																					//wait for a certain amount of time, constantly updating the inputs
 		update_inputs();
 	}
-		
-	TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_0);
-	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);
+	
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinLow(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
 	for( int i = 0; i<door_delay ; i++){
 		update_inputs();
 	}	
 		
-	TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_2);
-	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
 	for( int i = 0; i<door_delay ; i++){
 		update_inputs();
 	}	
 
+	TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
+	for( int i = 0; i<door_delay ; i++){
+		update_inputs();
+	}	
+	
 	return 0;
 }
 
-bool door_open_critical(void) {																								//doors open without updating buttons
+/*char door_open_critical(void) {																								//doors open without updating buttons
 	if(usart_on) {
 		usart_message("OPEN, doors, without update\n");
 	}
-	
+
 	door_closed = 0;
 		
 	TM_GPIO_SetPinLow(GPIOC, GPIO_Pin_11);																				
@@ -338,37 +399,49 @@ bool door_open_critical(void) {																								//doors open without upda
 
 	return 0;
 }
+*/
 
-
-bool door_close(void){
+char door_close(void){
 	if(usart_on) {
 		usart_message("CLOSE, doors\n");
 	}
-	
-	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_4);																					
-	TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_0);
-	for( int i = 0; i<door_delay ; i++){
-		update_inputs();
-	}
-		
-	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);
-	TM_GPIO_SetPinLow(GPIOC, GPIO_Pin_11);
+	TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
 	for( int i = 0; i<door_delay ; i++){
 		update_inputs();
 	}	
 		
-	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinLow(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
+	for( int i = 0; i<door_delay ; i++){
+		update_inputs();
+	}	
+		
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinLow(GPIOC, GPIO_Pin_11);	
+	TM_GPIO_SetPinHigh(GPIOA, GPIO_Pin_15);
+	for( int i = 0; i<door_delay ; i++){
+		update_inputs();
+	}		
+
+	TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_2);
+  TM_GPIO_SetPinHigh(GPIOD, GPIO_Pin_0);	
+	TM_GPIO_SetPinHigh(GPIOC, GPIO_Pin_11);	
 	TM_GPIO_SetPinLow(GPIOA, GPIO_Pin_15);
 	for( int i = 0; i<door_delay ; i++){
 		update_inputs();
 	}	
-
 	door_closed = 1;
 	
 	return 0;
 }
 
-bool door_cycle(void) {																													//open doors, wait, then close doors
+char door_cycle(void) {																													//open doors, wait, then close doors
 	door_open();
 	for( int i = 0; i< 12*door_delay; i++);	
 	door_close();
@@ -376,7 +449,7 @@ bool door_cycle(void) {																													//open doors, wait, then clo
 	return 0;
 }
 
-bool check_password(void){																			//TODO make this work
+/*char check_password(void){																			//TODO make this work
 	usart_message("Input Password\n");
 	TM_USART_Gets(USART1, pass_array, sizeof(pass_array));
 	
@@ -392,5 +465,33 @@ bool check_password(void){																			//TODO make this work
 	}
 	
 	return 0;
-}
+}*/
+
+char button_display(void) {
+	if(butt_f1) {
+		TM_GPIO_SetPinHigh(GPIOB, GPIO_Pin_11 );
+	} else {
+		TM_GPIO_SetPinLow(GPIOB, GPIO_Pin_11 );
+	}
+	
+	if(butt_f2_down) {
+		TM_GPIO_SetPinHigh(GPIOB, GPIO_Pin_12 );
+	} else {
+		TM_GPIO_SetPinLow(GPIOB, GPIO_Pin_12 );
+	}
+	
+	if(butt_f2_up) {
+		TM_GPIO_SetPinHigh(GPIOB, GPIO_Pin_14 );
+	} else {
+		TM_GPIO_SetPinLow(GPIOB, GPIO_Pin_14 );
+	}
+	
+	if(butt_f3) {
+		TM_GPIO_SetPinHigh(GPIOB, GPIO_Pin_10 );
+	} else {
+		TM_GPIO_SetPinLow(GPIOB, GPIO_Pin_10 );
+	}
+	
+	return 0;
+}	
 
--- MCU-Elavator/User/main.h
+++ MCU-Elavator/User/main.h
@@ -1,48 +1,47 @@
-#include <stdbool.h>
-
-typedef bool uint1_t;
-
-static bool init(void);
-static bool update_inputs(void);
-static bool update_critical(void);
-static bool activate_uart(void);
-static bool usart_message(char* str);
-static bool bcd_display(void);
 
 
-static bool door_open(void); 
-static bool door_open_critical(void); 
-static bool door_close(void);
-static bool door_cycle(void);
-static bool move_up(void);
-static bool move_down(void);
-static bool move_stop(void);
-static bool move_reset(void);
+static char init(void);
+static char update_inputs(void);
+static char update_critical(void);
+static char activate_uart(void);
+static char usart_message(char* str);
+static char bcd_display(void);
+static char button_display(void);
 
 
-const int door_delay = 15000000;
-const int motor_delay_1;													//TODO delete 1 and 3 if not needed
+static char door_open(void); 
+static char door_open_critical(void); 
+static char door_close(void);
+static char door_cycle(void);
+static char move_up(void);
+static char move_down(void);
+static char move_stop(void);
+static char move_reset(void);
+
+
+const int door_delay = 1500000;
+/*const int motor_delay_1;													//TODO delete 1 and 3 if not needed
 const int motor_delay_2 = 10000;									//TODO figure out value from physical model
-const int motor_delay_3;
+const int motor_delay_3;*/
 
 //TODO intialize all values
 static char loc_cur = 0;
-static bool loc_floor_1 = 0;
-static bool loc_floor_2 = 0;
-static bool loc_floor_3 = 0;
+static char loc_floor_1 = 0;
+static char loc_floor_2 = 0;
+static char loc_floor_3 = 0;
 
-static bool door_closed;
-static bool moving_up;
-static bool moving_down;
+static char door_closed;
+static char moving_up;
+static char moving_down;
 
-static bool butt_f1;
-static bool butt_f2_down;
-static bool butt_f2_up;
-static bool butt_f3;
-static bool butt_car_stop;
-static bool butt_car_reset;
+static char butt_f1;
+static char butt_f2_down;
+static char butt_f2_up;
+static char butt_f3;
+static char butt_car_stop;
+static char butt_car_reset;
 
-static bool usart_on = 0;
+static char usart_on = 0;
 const static char* password_correct = "MSE Rocks!";
 static char pass_array[12];
 
